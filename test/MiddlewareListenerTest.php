de4588f418f6c1ae5f63a196b896f74c42ec5655
Aleksei Khudiakov <aleksey@xerkus.pro>
2019-07-02 19:28:05 +1000

    Fix copyright formatting

--- test/MiddlewareListenerTest.php
+++ test/MiddlewareListenerTest.php
@@ -3,7 +3,6 @@
  * @see       https://github.com/zendframework/zend-mvc-middleware for the canonical source repository
  * @copyright Copyright (c) 2019 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   https://github.com/zendframework/zend-mvc-middleware/blob/master/LICENSE.md New BSD License
- *
  */
 
 namespace ZendTest\Mvc\Middleware;
